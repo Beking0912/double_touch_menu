a15205c0b26d3ae986498266774490d02eb2882d
Beking <sbqoo@qq.com>
2020-04-29 11:40:45 +0800

    add 初步完成效果

--- menu_demo/src/App.js
+++ menu_demo/src/App.js
@@ -1,16 +1,77 @@
 /*
- * @Author: beking 
- * @Date: 2020-04-28 11:10:10 
+ * @Author: beking
+ * @Date: 2020-04-28 11:10:10
  * @Last Modified by: beking
- * @Last Modified time: 2020-04-28 16:15:46
+ * @Last Modified time: 2020-04-29 11:39:03
  */
 
-
-import React from "react";
-import Menu from "./components/menu";
+import React, { useEffect } from "react";
+import Core from "@any-touch/core";
+import Tap from "@any-touch/tap";
+// import Menu from "./components/menu";
 
 function App() {
-  return <Menu></Menu>;
+  useEffect(() => {
+    Core.use(Tap, { pointLength: 2 });
+    const handler = document.getElementById("handler");
+    const at = new Core(handler);
+    const drawer = document.getElementById("drawer");
+    at.on("tap", (ev) => {
+      drawer.style.transform = "translateY(-40vh)";
+      drawer.style.transition = "all 500ms";
+    });
+  });
+
+  const hideMenu = () => {
+    Core.use(Tap, { pointLength: 1 });
+    const cancel = document.getElementById("cancel")
+    const att = new Core(cancel);
+    const drawer = document.getElementById("drawer");
+    att.on("tap", (ev) => {
+      drawer.style.transform = "translateY(40vh)";
+      drawer.style.transition = "all 500ms";
+    });
+  };
+
+  return (
+    <div style={{ height: "120vh" }}>
+      <div style={{ height: "100vh" }} id="handler"></div>
+      <div
+        style={{
+          height: "40vh",
+          width: "100%",
+          backgroundColor: "#F5F5F5",
+          display: "flex",
+          flexDirection: "column",
+        }}
+        id="drawer"
+      >
+        <div
+          style={{
+            height: "20%",
+            borderBottomStyle: "solid",
+            borderBottomColor: "#ced6e0",
+            borderBottomWidth: "1px",
+          }}
+        >
+          标题
+        </div>
+        <div style={{ flex: 1 }}>功能</div>
+        <div
+          style={{
+            height: "20%",
+            borderTopStyle: "solid",
+            borderTopColor: "#ced6e0",
+            borderTopWidth: "1px",
+          }}
+          id="cancel"
+          onClick={hideMenu}
+        >
+          取消
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- menu_demo/src/components/menu.js
+++ menu_demo/src/components/menu.js
@@ -2,7 +2,7 @@
  * @Author: beking
  * @Date: 2020-04-28 11:10:15
  * @Last Modified by: beking
- * @Last Modified time: 2020-04-28 18:35:05
+ * @Last Modified time: 2020-04-29 10:50:14
  */
 
 import React, { Component } from "react";
@@ -11,35 +11,46 @@ import Core from "@any-touch/core";
 import Tap from "@any-touch/tap";
 
 class Menu extends Component {
-  showMenu = () => {
-    Core.use(Tap, { pointLength: 2 });
+  componentDidMount() {
+    Core.use(Tap, { pointLength: 2 }); // 双指点击
     const at = new Core(this.handler);
     at.on("tap", (ev) => {
-      this.drawer.style.transform = "translateY(40vh)";
-      this.drawer.style.transition = "all 200ms";
+      this.drawer.style.transform = "translateY(-40vh)";
+      this.drawer.style.transition = "all 500ms";
     });
+  }
+
+  showMenu = () => {
+    // Core.use(Tap, { pointLength: 2 }); // 双指点击
+    // const at = new Core(this.handler);
+    // at.on("tap", (ev) => {
+    //   this.drawer.style.transform = "translateY(-40vh)";
+    //   this.drawer.style.transition = "all 500ms";
+    // });
   };
 
   hideMenu = () => {
-    Core.use(Tap);
-    const att = new Core(this.cancel);
-    att.on("tap", (ev) => {
-      this.drawer.style.transform = "translateY(-40vh)";
-      this.drawer.style.transition = "all 200ms";
-    });
+    this.drawer.style.backgroundColor = 'red'
+    // Core.use(Tap, { pointLength: 1 });
+    // const att = new Core(this.cancel);
+    // att.on("tap", (ev) => {
+    //   this.drawer.style.transform = "translateY(40vh)";
+    //   this.drawer.style.transition = "all 500ms";
+    // });
   };
 
   render() {
     return (
       <div
-        style={{ height: "100vh" }}
+        style={{ height: "120vh" }}
         ref={(ref) => {
           this.handler = ref;
         }}
-        onClick={this.showMenu}
+        // onClick={this.showMenu} // 点击一次后再用双指点击才能有效果
       >
+        <div style={{ height: "100vh" }}></div>
         <div
-          style={{   // 这要如何定位呢？
+          style={{
             height: "40vh",
             width: "100%",
             backgroundColor: "#F5F5F5",
@@ -71,7 +82,7 @@ class Menu extends Component {
             ref={(ref) => {
               this.cancel = ref;
             }}
-            onClick={this.hideMenu}  // 为啥没反应呢？
+            onClick={this.hideMenu} 
           >
             取消
           </div>
